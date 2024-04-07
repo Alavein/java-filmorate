7598a4ba0c5f3554627ba6247690d1798a4ecd07
Полина Орлова <cantatcaelum@yandex.ru>
2024-04-07 23:47:25 +0300

    style: убрать лишние импорты

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -16,7 +16,6 @@ import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
